581744aab011942e0d1ff6024a72c635df99be3b
Javier Olaechea <pirata@gmail.com>
2022-12-24 21:27:33 -0500

    Remove Windows support from mudix.c

--- src/mudix.c
+++ src/mudix.c
@@ -18,78 +18,17 @@
 
 #include <stdio.h>
 #include <signal.h>
-#if !defined(WIN32)
-  #include <sys/wait.h>
-#else
-  #include <io.h>
-  #include <windows.h>
-  #include <winbase.h>
-#endif
+#include <sys/wait.h>
 #include "mudix.h"
 
-
-void signal_handler(int signal)
-{
-#if !defined(WIN32)
+void signal_handler(int signal) {
     /* just call wait with no hang so that we get no zombies */
     waitpid(-1, NULL, WNOHANG);
-#endif
 }
 
-
-#if !defined(WIN32)
-/*
- * Main loop
- */
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     /* install a signal for SIGCHLD for when a child dies */
     signal(SIGCHLD, signal_handler);
-
     init_gui(argc, argv);
-
     return 0;
 }
-
-#else
-
-int WINAPI WinMain(HINSTANCE hInstance,  // handle to current instance
-                   HINSTANCE hPrevInstance,  // handle to previous instance
-                   LPSTR lpCmdLine,      // pointer to command line
-                   int nCmdShow)         // show state of window
-{
-    WORD wVersionRequested;
-    WSADATA wsaData;
-    int err;
- 
-    wVersionRequested = MAKEWORD(2, 2);
- 
-    err = WSAStartup(wVersionRequested, &wsaData);
-    if (err != 0) 
-    {
-        /* Tell the user that we could not find a usable */
-        /* WinSock DLL.                                  */
-        return 0;
-    }
- 
-    /* Confirm that the WinSock DLL supports 2.2.*/
-    /* Note that if the DLL supports versions greater    */
-    /* than 2.2 in addition to 2.2, it will still return */
-    /* 2.2 in wVersion since that is the version we      */
-    /* requested.                                        */
- 
-    if (LOBYTE(wsaData.wVersion) != 2 ||
-        HIBYTE(wsaData.wVersion) != 2) 
-    {
-        /* Tell the user that we could not find a usable */
-        /* WinSock DLL.                                  */
-        WSACleanup();
-        return 0; 
-    }
-        
-    init_gui(0, NULL);
-
-    return 0;
-}
-
-#endif
