e73bb50573cdb9523da91b2825056d6cad7fa154
Javier Olaechea <pirata@gmail.com>
2022-12-25 20:29:14 -0500

    Add conf.h header

---
+++ include/conf.h
@@ -0,0 +1,5 @@
+// TODO(javier): This file is a stand-in for the one generated from
+// autoconf. Should move the two defines mudix.h and delete this file in the
+// future
+#define PACKAGE "gmudix"
+#define VERSION "1.0"
