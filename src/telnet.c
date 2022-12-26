8d90e12be45893e39f8e86eefa1873bf059c1b57
Javier Olaechea <pirata@gmail.com>
2022-12-26 16:43:43 -0500

    Remove windows support from telnet.c

--- src/telnet.c
+++ src/telnet.c
@@ -19,71 +19,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#if !defined(WIN32)
-  #include <unistd.h>
-  #ifdef IAC_DEBUG
-    #define TELCMDS
-    #define TELOPTS
-  #endif
-  #include <arpa/telnet.h>    /* has to be after IAC_DEBUG */
-#else
-  #include <io.h>
-  #include <windows.h>
-  #include <winbase.h>
+#include <unistd.h>
+#ifdef IAC_DEBUG
+  #define TELCMDS
+  #define TELOPTS
 #endif
+#include <arpa/telnet.h>    /* has to be after IAC_DEBUG */
 #include "mudix.h"
-
-#if defined(WIN32)
-  /* telnet options */
-  #define TELOPT_BINARY         0       /* 8-bit data path */
-  #define TELOPT_ECHO           1       /* echo */
-  #define TELOPT_RCP            2       /* prepare to reconnect */
-  #define TELOPT_SGA            3       /* suppress go ahead */
-  #define TELOPT_NAMS           4       /* approximate message size */
-  #define TELOPT_STATUS         5       /* give status */
-  #define TELOPT_TM             6       /* timing mark */
-  #define TELOPT_RCTE           7       /* remote controlled transmission and echo */
-  #define TELOPT_NAOL           8       /* negotiate about output line width */
-  #define TELOPT_NAOP           9       /* negotiate about output page size */
-  #define TELOPT_NAOCRD         10      /* negotiate about CR disposition */
-  #define TELOPT_NAOHTS         11      /* negotiate about horizontal tabstops */
-  #define TELOPT_NAOHTD         12      /* negotiate about horizontal tab disposition */
-  #define TELOPT_NAOFFD         13      /* negotiate about formfeed disposition */
-  #define TELOPT_NAOVTS         14      /* negotiate about vertical tab stops */
-  #define TELOPT_NAOVTD         15      /* negotiate about vertical tab disposition */
-  #define TELOPT_NAOLFD         16      /* negotiate about output LF disposition */
-  #define TELOPT_XASCII         17      /* extended ascii character set */
-  #define TELOPT_LOGOUT         18      /* force logout */
-  #define TELOPT_BM             19      /* byte macro */
-  #define TELOPT_DET            20      /* data entry terminal */
-  #define TELOPT_SUPDUP         21      /* supdup protocol */
-  #define TELOPT_SUPDUPOUTPUT   22      /* supdup output */
-  #define TELOPT_SNDLOC         23      /* send location */
-  #define TELOPT_TTYPE          24      /* terminal type */
-  #define TELOPT_EOR            25      /* end or record */
-  #define TELOPT_TUID           26      /* TACACS user identification */
-  #define TELOPT_OUTMRK         27      /* output marking */
-  #define TELOPT_TTYLOC         28      /* terminal location number */
-  #define TELOPT_3270REGIME     29      /* 3270 regime */
-  #define TELOPT_X3PAD          30      /* X.3 PAD */
-  #define TELOPT_NAWS           31      /* window size */
-  #define TELOPT_TSPEED         32      /* terminal speed */
-  #define TELOPT_LFLOW          33      /* remote flow control */
-  #define TELOPT_LINEMODE       34      /* Linemode option */
-  #define TELOPT_XDISPLOC       35      /* X Display Location */
-  #define TELOPT_OLD_ENVIRON    36      /* Old - Environment variables */
-  #define TELOPT_AUTHENTICATION 37      /* Authenticate */
-  #define TELOPT_ENCRYPT        38      /* Encryption option */
-  #define TELOPT_NEW_ENVIRON    39      /* New - Environment variables */
-  #define TELOPT_EXOPL          255     /* extended-options-list */
-
-  /* sub-option qualifiers */
-  #define TELQUAL_IS            0       /* option is... */
-  #define TELQUAL_SEND          1       /* send option */
-  #define TELQUAL_INFO          2       /* ENVIRON: informational version of IS */
-  #define TELQUAL_REPLY         2       /* AUTHENTICATION: client version of IS */
-  #define TELQUAL_NAME          3       /* AUTHENTICATION: client version of IS */
-#endif
 
 /* mccp */
 #define TELOPT_COMPRESS        (85)
@@ -107,13 +49,7 @@ static void send_telnet(USER *user, unsigned char command, unsigned char option)
 
     /* lock the mutex */
     g_mutex_lock(user_network_mutex);
-
-#if !defined(WIN32)
     write(user->net.sock, iac_buf, 3); /* bypassing normal send */
-#else
-    send(user->net.sock, iac_buf, 3, 0); /* bypassing normal send */
-#endif
-
     /* unlock the mutex */
     g_mutex_unlock(user_network_mutex);
 }
